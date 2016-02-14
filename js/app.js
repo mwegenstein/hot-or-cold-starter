4c710bb5e7389a97d11b9d450db21bfadd7bb4c7
Michelle Wegenstein <michelle@looker.com>
2016-02-14 12:42:58 -0800

    tidy up first steps

--- js/app.js
+++ js/app.js
@@ -18,30 +18,12 @@ $(document).ready(function(){
 //The user should get feedback about each guess – if it was too low, too high, or just right. This means that you'll need to write a named function that takes a user guess and determines which feedback to provide.
 
 //ToDo
-// 1. I generate a number between 1-100
-// 2. user guesses the number and submits it. 
-// 3. I take the submitions and compare it to the generated result
+// 1. I generate a number between 1-100 CHECK
+// 2. user guesses the number and submits it. CHECK
+// 3. I take the submitions and compare it to the generated result CHECK
 // 4. Decide hot or cold or is the number. prompt to guess again
 
-// generate a number from 1-100 randomized. 
 
-// $("#guessButton").click(function(){
-//  var guessValue = $(this).closest("form").find("input[name='userGuess']").val();//this is now global variable
-
-//  function generateInt() {
-//     var myNum = Math.floor(Math.random() * 100) + 1
-//     return myNum
-//     console.log(myNum)
-//   }; generateInt();  
-
-
-//   function compareValues(a, b) { 
-//     var compare = Math.abs(a - b);
-//     console.log("hi"); 
-//   };
-//   compareValues(guessValue, generateInt());
-
-// }); 
 
 function generateInt() {
   var myNum = Math.floor(Math.random() * 100) + 1;
@@ -51,7 +33,7 @@ function generateInt() {
 
 $("#guessButton").click(function() {
   var guessValue = $("#userGuess").val();
-  return false;
+  // return false;
   console.log(guessValue);
   function compareValues(a, b) {
     var compare = Math.abs(a - b);
@@ -61,11 +43,6 @@ $("#guessButton").click(function() {
   compareValues(guessValue, generateInt());
 });
 
-
-
-
-// compareValues(guessValue, generateInt());
-//HOW DO I COMPARE THE TWO VALUES?
 
 
 //RESTART GAME/New Game
